ea55569e4c2211b01be147fdbb813e2afa0f7b04
lucas123045 <lucaveiga12@gmail.com>
2026-03-17 11:40:23 -0300

    "commit lucas 5"

--- main.c
+++ main.c
@@ -1,15 +1,9 @@
 #include<stdio.h>
 
-<<<<<<< HEAD
-int main ()(
-    print ("lucas klein, MAtheus Dagel");
-
-    return 0;
-)
-=======
 int main(){
     printf("João Pedro Quagliano");
-
+    printf("lucas klein");
     return 0;
 }
->>>>>>> 612a99aef0c3a45ec580353a10e58ccceb80234d
+
+
