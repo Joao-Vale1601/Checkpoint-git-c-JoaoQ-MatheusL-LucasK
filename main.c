becf72a9e87378272763af5f38996d1ea5e91337
lucas123045 <lucaveiga12@gmail.com>
2026-03-17 11:48:12 -0300

    "commit todo mundo"

--- main.c
+++ main.c
@@ -1,13 +1,4 @@
 #include<stdio.h>
-
-int main(){
-<<<<<<< HEAD
-    printf("Matheus Levi");
-
-    return 0;
-}
-
-=======
 int main(){
     printf("João Pedro Quagliano");
     printf("lucas klein");
@@ -16,4 +7,3 @@ int main(){
 }
 
 
->>>>>>> ea55569e4c2211b01be147fdbb813e2afa0f7b04
